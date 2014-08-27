3da2e9cdc86630e11497a602777dc17cf408f4d3
thondery <thondery@163.com>
2014-08-27 17:18:30 +0800

    更改数据模型样式...

--- app/model/user.js
+++ app/model/user.js
@@ -1,26 +1,18 @@
 /**
  * Created by park on 2014/8/7.
+ * Users --> 用户数据模型
  */
 
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-var util = require('util');
-
-var daoDoc = {
+module.exports = {
     username: String,
     password: String,
     email: String,
     regip: { type: Number, default: 0 },
-    regdate: { type: Number, default: Db.timestamp() },
+    regdate: { type: Number, default: Date.now() },
     lastip: { type: Number, default: 0 },
-    lastime: { type: Number, default: Db.timestamp() },
+    lastime: { type: Number, default: Date.now() },
     salt: String,
     locked: { type: Boolean, default: false },
     flag: { type: String, default: '' },
     group: { type: String, default: '' }
 };
-
-var daoSchema = new Schema(daoDoc);
-
-var daoModel = global.Db.model(util.format('%susers', global.Db._prefix), daoSchema);
-module.exports = daoModel;
--- library/model.js
+++ library/model.js
@@ -1,25 +1,30 @@
 /**
  * Created by chenggang on 14-6-6.
+ * 数据模型调用
  */
 
 var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-var util = require('util');
-var path = require('path');
-var _ = require('underscore');
+var path     = require('path');
+var util     = require('util');
+var _        = require('underscore');
 
-module.exports = function(name, _doc) {
-
-    if (_doc && _.isObject(_doc)) {
-        var _schema = new Schema(_doc);
-        var _model = global.Db.model(util.format('%s%s', global.Db._prefix, name), _schema);
-        return _model;
-    }
-
-    var dao = require(path.join(global.appDir, global.Db._model.path, name));
-
-
-    if (!_doc) return dao;
-
-    return this;
+module.exports = function (name, doc) {
+    var _doc = doc
+        || require(
+            path.join(
+                global.appDir,
+                global.Db._model.path,
+                name
+            )
+        );
+    var _schema = new mongoose.Schema(_doc);
+    var _model = global.Db.model(
+        util.format(
+            '%s%s',
+            global.Db._prefix,
+            name
+        ),
+        _schema
+    );
+    return _model;
 };
